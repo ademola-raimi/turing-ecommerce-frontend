17e6f97845354bd2948492cbdbee2147e9bf0ee6
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-07-15 23:13:21 +0100

    add actions and config

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+//written in CommonJS so the constants can be used in setupProxy.js
+module.exports = {
+    api_path: 'https://turing-demo.herokuapp.com',
+    version_path: '/api/v1',
+    products_path: '/products',
+    cloudinary_path: 'https://res.cloudinary.com/demola/image/upload/v1563150887/turing/',
+    categories_path: '/categories',
+    // roomType_path: '/roomtype',
+    // rooms_path: '/rooms',
+    // room_path: '/room',
+    // priceLists_path: '/pricelists',
+    // priceList_path: '/pricelist'
+}

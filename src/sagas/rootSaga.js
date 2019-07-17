a07a4cca20eff2a12abf9f41ab0e8ef5cee5c0bd
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-07-17 14:11:12 +0100

    register new saga file

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,12 +1,14 @@
 import { all, fork } from 'redux-saga/effects';
 import * as ProductSagas from './Products';
 import * as CategorySagas from './Categories';
+import * as ShoppingCartSagas from './ShoppingCart';
 
 // import watchers from other files
 export default function* rootSaga() {
   yield all([
         ...Object.values(ProductSagas),
-        ...Object.values(CategorySagas)
+        ...Object.values(CategorySagas),
+        ...Object.values(ShoppingCartSagas)
         // add other watchers to the array
     ].map(fork));
 }

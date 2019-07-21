5934c43a6e45ab39cc3a167b627b159f6e8f978e
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-07-21 04:00:33 +0100

    register customer, shoppingcart, shipping and order saga in root saga

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -2,13 +2,19 @@ import { all, fork } from 'redux-saga/effects';
 import * as ProductSagas from './Products';
 import * as CategorySagas from './Categories';
 import * as ShoppingCartSagas from './ShoppingCart';
+import * as CustomersSagas from './Customers';
+import * as OrderSagas from './Order';
+import * as ShippingSagas from './Shipping';
 
 // import watchers from other files
 export default function* rootSaga() {
   yield all([
         ...Object.values(ProductSagas),
         ...Object.values(CategorySagas),
-        ...Object.values(ShoppingCartSagas)
+        ...Object.values(ShoppingCartSagas),
+        ...Object.values(CustomersSagas),
+        ...Object.values(OrderSagas),
+        ...Object.values(ShippingSagas)
         // add other watchers to the array
     ].map(fork));
 }

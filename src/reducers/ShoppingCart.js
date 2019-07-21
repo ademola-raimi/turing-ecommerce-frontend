a765cf043554aa86ecba71b6ceb1af71e64fcfd1
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-07-21 03:58:03 +0100

    reducer for all thing cart

--- src/reducers/ShoppingCart.js
+++ src/reducers/ShoppingCart.js
@@ -10,7 +10,16 @@ import {
     TOTAL_AMOUNT_FAILED,
     FETCH_CART_COUNT,
     FETCH_CART_COUNT_RECIEVED,
-    FECTH_CART_COUNT_FAILED
+    FECTH_CART_COUNT_FAILED,
+    FETCH_ALL_CARTS,
+    FETCH_ALL_CARTS_RECEIVED,
+    FETCH_ALL_CARTS_FAILED,
+    EMPTY_CARTS,
+    EMPTY_CARTS_RECEIVED,
+    EMPTY_CARTS_FAILED,
+    REMOVE_PRODUCT,
+    REMOVE_PRODUCT_RECEIVED,
+    REMOVE_PRODUCT_FAILED
 } from '../actions/types';
 
 import _ from 'lodash';
@@ -22,40 +31,65 @@ const initialState = {
     cartId: '',
     totalAmount: 0,
     totalCartItem: 0,
+    allCarts: [],
+    isBasketEmpty: false,
+    removeProduct: false
   }
 
 export default function categoryReducer(state = initialState, action) {
     let newState = state;
     switch (action.type) {
         case FETCH_CART_ID_RECEIVED:
-            console.log('cart_id:',action.response.data.cart_id)
             newState = Object.assign({}, state, { isLoading: false, cartId: action.response.data.cart_id });
             return newState;
 
         case SAVE_CART_RECIEVED:
-        console.log('cart add: ',action.response.data)
-            localStorage.setItem("itemId", action.response.data.item_id);
+            localStorage.setItem("cartsInfo", JSON.stringify(action.response.data));
             newState = Object.assign({}, state, { isLoading: false} );
             return newState;
 
         case TOTAL_AMOUNT_RECIEVED:
-            console.log('total cart amt: ',action.response.data.total_amount.toFixed(2))
 
             newState = Object.assign({}, state, { isLoading: false, totalAmount: action.response.data.total_amount.toFixed(2) });
             return newState;
 
         case FETCH_CART_COUNT_RECIEVED:
-            console.log('total cart count: ',action.response.data.total_count);
 
             newState = Object.assign({}, state, { isLoading: false, totalCartItem: action.response.data.total_count });
             return newState;
 
+        case FETCH_ALL_CARTS_RECEIVED:
+            console.log('all carts: ',action.response.data)
+            const isBasketEmpty = action.response.data.length > 0 ? false : true;
+            newState = Object.assign({}, state, { isLoading: false, allCarts: action.response.data, isBasketEmpty: isBasketEmpty, removeProduct: false});
+            return newState;
+
+        case EMPTY_CARTS_RECEIVED:
+            console.log('empty cart received: ',action.response.data)
+            newState = Object.assign({}, state, { isLoading: false, allCarts: [], isBasketEmpty: true, totalCartItem: 0, totalAmount: 0});
+            return newState;
+
+        case REMOVE_PRODUCT_RECEIVED:
+            newState = Object.assign({}, state, { isLoading: false, removeProduct: true });
+            return newState;
+
+        case FETCH_ALL_CARTS_FAILED:
+            // console.log('git here: ',action.response.data)
+            newState = Object.assign({}, state, { isLoading: false, isBasketEmpty: true});
+            return newState;
+
+        case REMOVE_PRODUCT:
+        case EMPTY_CARTS:
+        case FETCH_ALL_CARTS:
         case FETCH_CART_COUNT:
         case SAVE_CART:
         case FETCH_TOTAL_AMOUNT:
         case FETCH_CART_ID:
             return state;
 
+        case REMOVE_PRODUCT_FAILED:
+        case EMPTY_CARTS_FAILED:
+        case FETCH_ALL_CARTS_FAILED:
         case FECTH_CART_COUNT_FAILED:
         case FETCH_CART_ID_FAILED:
         case TOTAL_AMOUNT_FAILED:

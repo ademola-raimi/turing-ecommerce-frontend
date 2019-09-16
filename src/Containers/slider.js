1f2c1868206ebbc21b7c545072b18d5c3f56bb30
Ademola <ademolaraimi.nig@gmail.com>
2019-09-16 03:12:42 +0100

    add slider for price range filter

---
+++ src/Containers/slider.js
@@ -0,0 +1,86 @@
+export default {
+  root: {
+    width: '50%',
+    marginTop: 50,
+    marginBottom: 20,
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  description: {
+    color: '#545252',
+  },
+  label: {
+    marginBottom: 10,
+  },
+  valueText: {
+    color: '#000066',
+    fontWeight: '600',
+    margin: '5px 5px 0',
+  },
+  code: {
+    margin: '10px 0',
+    textAlign: 'left',
+  },
+  info: {
+    fontStyle: 'italic',
+    color: '#3131d4',
+    fontSize: 15,
+    margin: '5px 0',
+  },
+  sliderWrapper: {
+    width: '75%',
+  },
+  slider: {
+    height: 40,
+    backgroundColor: '#f62f5e',
+    border: '1px solid #f62f5e',
+  },
+  trackStyle: {
+    height: 5,
+    border: '3px solid black',
+    backgroundColor: 'white',
+    top: 14,
+  },
+  highlightedTrackStyle: {
+    height: 5,
+    border: '3px solid black',
+    backgroundColor: 'grey',
+    top: 14,
+  },
+  handleStyle: {
+    height: 20,
+    width: 20,
+    border: '1px solid #f62f5e',
+    backgroundColor: '#f62f5e',
+    top: 5,
+    outline: 'none',
+  },
+  hoveredHandleStyle: {
+    backgroundColor: '#f62f5e',
+    border: '1px solid #f62f5e',
+    boxShadow: '0px 0px 5px 0px #dc0d0d',
+  },
+  activeHandleStyle: {
+    backgroundColor: '#f62f5e',
+    border: '1px solid #f62f5e',
+    boxShadow: 'inset 0px 0px 5px 0px #dc0d0d',
+  },
+  focusedHandleStyle: {
+    boxShadow: '0px 0px 5px 0px #dc0d0d',
+    border: '1px solid #f62f5e',
+  },
+  sliderTrack: {
+    backgroundColor: '#f62f5e',
+    border: '1px solid #f62f5e',
+  },
+  sliderHighlightedTrack: {
+    backgroundColor: '#f62f5e',
+    border: '1px solid #f62f5e',
+  },
+  sliderHandle: {
+    backgroundColor: '#f62f5e',
+    border: '1px solid #f62f5e',
+  },
+};

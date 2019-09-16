c3a35535ac74f034b0c202b6a841851ef68afd80
Ademola <ademolaraimi.nig@gmail.com>
2019-09-16 03:30:19 +0100

    restructure sidebar to add filter by attributes

--- src/Containers/SideBar.js
+++ src/Containers/SideBar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import BasketCart from './BasketCart';
 import Search from './Search';
+import Attributes from './Attributes';
 import Categories from './Categories';
 import Departments from './Departments';
 
 export const SideBar = ()=>{
     return(
-        <div>
+        <div className="side">
             <BasketCart/>
             <Search/>
+            <Attributes/>
             <Categories/>
             <Departments/>
         </div>
